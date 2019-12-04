cf2a3c627fc2d84d52c9fbde3d77ff069da570bb
Guillermo García Rojas C <solobsd@solobsd.org>
2019-12-04 10:20:45 -0600

    Corrección de índice

--- Amigables.py
+++ Amigables.py
@@ -7,10 +7,10 @@ sum2 = 0
 for i in range (1,x):
     if x%i==0:
         sum1+=i
-for j in range (i,y):
+for j in range (1,y):
     if y%j==0:
         sum2+=j
 if(sum1==y and sum2==x):
-    print('Amigable!')
+    print('Amigables!')
 else:
-    print('No Amigable!')
+    print('No Amigables!')
